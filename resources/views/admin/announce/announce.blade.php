035600c97867a09bfe9d69d8e6dc5d2d19d1a008
juntiwa <juntima.nuch@gmail.com>
2022-11-15 10:53:20 +0700

    change format icon

--- resources/views/admin/announce/announce.blade.php
+++ resources/views/admin/announce/announce.blade.php
@@ -51,7 +51,7 @@
                                     @csrf
                                     @if($announce->statuses === "true")
                                         <label class="cursor-pointer">
-                                            <input type="submit" name="statuses" id="statuses" value="true" hidden>
+                                            <input type="submit" name="statuses" id="statuses" value="false" hidden>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="#2dd4bf" viewBox="0 0 24 24"
                                                  stroke-width="1.5"
                                                  stroke="currentColor" class="w-6 h-6">
@@ -63,7 +63,7 @@
                                         </label>
                                     @else
                                     <label class="cursor-pointer">
-                                            <input type="submit" name="statuses" id="statuses" value="false" hidden>
+                                            <input type="submit" name="statuses" id="statuses" value="true" hidden>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="#fb7185" viewBox="0 0 24 24"
                                                  stroke-width="1.5"
                                                  stroke="currentColor" class="w-6 h-6">
